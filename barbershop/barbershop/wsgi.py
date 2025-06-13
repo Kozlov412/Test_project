f503637177314eafb4771cc335038e9e1de42727
Vadim <kozlov67b@gmail.com>
2025-06-13 15:38:59 +0300

    Отказ от скриптов

--- barbershop/barbershop/wsgi.py
+++ barbershop/barbershop/wsgi.py
@@ -8,20 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from django.core.wsgi import get_wsgi_application
 
-# Добавим отладочные сообщения
-print("Загрузка WSGI приложения...")
-sys.stdout.flush()
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barbershop.settings')
 
-try:
-    application = get_wsgi_application()
-    print("WSGI приложение успешно загружено")
-    sys.stdout.flush()
-except Exception as e:
-    print(f"Ошибка при загрузке WSGI приложения: {e}", file=sys.stderr)
-    sys.stderr.flush()
-    raise
+application = get_wsgi_application()
