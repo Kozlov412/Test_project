36810b29477ef806e397cf69007d6909139cad68
Vadim <kozlov67b@gmail.com>
2025-06-13 15:35:56 +0300

    Попытка решения Проблемы с gunicorn

--- barbershop/barbershop/wsgi.py
+++ barbershop/barbershop/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Добавим отладочные сообщения
+print("Загрузка WSGI приложения...")
+sys.stdout.flush()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barbershop.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print("WSGI приложение успешно загружено")
+    sys.stdout.flush()
+except Exception as e:
+    print(f"Ошибка при загрузке WSGI приложения: {e}", file=sys.stderr)
+    sys.stderr.flush()
+    raise
