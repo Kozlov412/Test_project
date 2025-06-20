74e2973c45c13f7485afb79cc1a007806e9cc641
Vadim <kozlov67b@gmail.com>
2025-06-20 14:24:59 +0300

    Добавление фейковой миграции

---
+++ barbershop/core/migrations/0006_empty_fix_migration.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('core', '0005_merge_20250529_1737'),
+    ]
+
+    operations = [
+        
+    ]
